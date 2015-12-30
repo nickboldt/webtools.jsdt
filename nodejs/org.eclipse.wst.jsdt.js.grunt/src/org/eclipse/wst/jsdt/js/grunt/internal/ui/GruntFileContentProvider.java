080294b3aa6a114742eee3030829f718a2f0a7d8
Ilya Buziuk <ilyabuziuk@gmail.com>
2015-12-30 17:42:51 +0300

    Bug_483398: Initial CNF support

---
+++ nodejs/org.eclipse.wst.jsdt.js.grunt/src/org/eclipse/wst/jsdt/js/grunt/internal/ui/GruntFileContentProvider.java
@@ -0,0 +1,182 @@
+package org.eclipse.wst.jsdt.js.grunt.internal.ui;
+
+import java.util.List;
+import java.util.Set;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceChangeEvent;
+import org.eclipse.core.resources.IResourceChangeListener;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.IResourceDeltaVisitor;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.ui.IMemento;
+import org.eclipse.ui.navigator.ICommonContentExtensionSite;
+import org.eclipse.ui.navigator.IPipelinedTreeContentProvider;
+import org.eclipse.ui.navigator.PipelinedShapeModification;
+import org.eclipse.ui.navigator.PipelinedViewerUpdate;
+
+public class GruntFileContentProvider
+		implements ITreeContentProvider, IPipelinedTreeContentProvider, IResourceChangeListener, IResourceDeltaVisitor {
+
+	protected static final Object[] EMPTY_ARRAY = new Object[0];
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.jface.viewers.IContentProvider#dispose()
+	 */
+	@Override
+	public void dispose() {
+		// do nothing
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.jface.viewers.IContentProvider#inputChanged(org.eclipse.jface
+	 * .viewers.Viewer, java.lang.Object, java.lang.Object)
+	 */
+	@Override
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+		// do nothing
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.jface.viewers.ITreeContentProvider#getChildren(Object)
+	 */
+	@Override
+	public Object[] getChildren(Object parentNode) {
+		// IJSBuildFileNode buildFileNode = null;
+		// if (parentNode instanceof IResource) {
+		// buildFileNode = JSBuildFileFactoryManager
+		// .tryToCreate((IResource) parentNode);
+		// } else if (parentNode instanceof IJSBuildFileNode) {
+		// buildFileNode = (IJSBuildFileNode) parentNode;
+		// }
+		// return JSBuildFileUtil.getTasks(buildFileNode).toArray();
+		return new String[]{"Hello"};
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.jface.viewers.ITreeContentProvider#getParent(Object)
+	 */
+	@Override
+	public Object getParent(Object element) {
+		// if (element instanceof IJSBuildFileNode) {
+		// return ((IJSBuildFileNode) element).getParentNode();
+		// }
+		// return null;
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.jface.viewers.ITreeContentProvider#hasChildren(Object)
+	 */
+	@Override
+	public boolean hasChildren(Object element) {
+		// if (element instanceof IJSBuildFileNode) {
+		// return ((IJSBuildFileNode) element).hasChildren();
+		// }
+		// return true;
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.jface.viewers.IStructuredContentProvider#getElements(Object)
+	 */
+	@Override
+	public Object[] getElements(Object inputElement) {
+		if (inputElement instanceof List) {
+			return ((List<?>) inputElement).toArray();
+		}
+		if (inputElement instanceof Object[]) {
+			return (Object[]) inputElement;
+		}
+		return EMPTY_ARRAY;
+	}
+
+	@Override
+	public boolean visit(IResourceDelta arg0) throws CoreException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void resourceChanged(IResourceChangeEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(ICommonContentExtensionSite arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void restoreState(IMemento arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void saveState(IMemento arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getPipelinedChildren(Object arg0, Set arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getPipelinedElements(Object arg0, Set arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object getPipelinedParent(Object arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PipelinedShapeModification interceptAdd(PipelinedShapeModification arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean interceptRefresh(PipelinedViewerUpdate arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public PipelinedShapeModification interceptRemove(PipelinedShapeModification arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean interceptUpdate(PipelinedViewerUpdate arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ nodejs/org.eclipse.wst.jsdt.js.grunt/src/org/eclipse/wst/jsdt/js/grunt/internal/ui/GruntLabelProvider.java
@@ -0,0 +1,86 @@
+package org.eclipse.wst.jsdt.js.grunt.internal.ui;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.viewers.IColorProvider;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.StyledString;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
+
+
+import org.eclipse.jface.viewers.DelegatingStyledCellLabelProvider.IStyledLabelProvider;
+
+public class GruntLabelProvider extends LabelProvider implements
+IStyledLabelProvider, IColorProvider{
+	
+//	@Override
+//	public StyledString getStyledText(Object element) {
+//		if (element instanceof IJSBuildFile) {
+//			IJSBuildFile node = (IJSBuildFile) element;
+//			StyledString buff = new StyledString(node.getLabel());
+//			IFile buildfile = node.getBuildFileResource();
+//			if (buildfile != null) {
+//				buff.append("  "); //$NON-NLS-1$
+//				buff.append('[', StyledString.DECORATIONS_STYLER);
+//				buff.append(buildfile.getFullPath().makeRelative().toString(),
+//						StyledString.DECORATIONS_STYLER);
+//				buff.append(']', StyledString.DECORATIONS_STYLER);
+//			}
+//			return buff;
+//		} else if (element instanceof ITask) {
+//			return new StyledString(((ITask) element).getLabel());
+//		}
+//		return null;
+//	}
+//
+//	@Override
+//	public Color getForeground(Object node) {
+//		if (node instanceof ITask && ((ITask) node).isDefault()) {
+//			return Display.getDefault().getSystemColor(SWT.COLOR_BLUE);
+//		}
+//		return Display.getDefault().getSystemColor(SWT.COLOR_LIST_FOREGROUND);
+//	}
+//
+//	@Override
+//	public Color getBackground(Object element) {
+//		return Display.getDefault().getSystemColor(SWT.COLOR_LIST_BACKGROUND);
+//	}
+
+	@Override
+	public Image getImage(Object element) {
+//		if (element instanceof IJSBuildFileNode) {
+//			String factoryId = ((IJSBuildFileNode) element).getFactoryId();
+//			ILabelProvider labelProvider = LabelProviderRegistryReader
+//					.getInstance().getLabelProvider(factoryId);
+//			if (labelProvider != null) {
+//				// custom image.
+//				Image image = labelProvider.getImage(element);
+//				if (image != null) {
+//					return image;
+//				}
+//			}
+//		}
+//		return super.getImage(element);
+		return null;
+	}
+
+
+	@Override
+	public Color getBackground(Object arg0) {
+		return Display.getDefault().getSystemColor(SWT.COLOR_LIST_BACKGROUND);
+	}
+
+	@Override
+	public Color getForeground(Object arg0) {
+		return Display.getDefault().getSystemColor(SWT.COLOR_LIST_FOREGROUND);
+	}
+
+	@Override
+	public StyledString getStyledText(Object arg0) {
+		return new StyledString("Hello!");
+	}
+
+}
